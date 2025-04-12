a2b0878130f7bcad96e22368fd8249c471e0b66c
Nileshaludiya <nileshaludiya@gmail.com>
2025-04-12 13:50:50 -0700

    modify

--- tutorial6.py
+++ tutorial6.py
@@ -1,3 +1,7 @@
+# Write a program to input three numbers(A, B & C) from user and print the maximum element among A, B & C in each line.		
+# Problem Constraints		
+# 1 <= A <= 1000000		
+
 a = float(input("Enter first number :"))
 b = int(input("Enter second number :"))
 c = float(input("Enter third number :"))
